0a1dc5d6a3ec4ef4b52ff91a27253bd3ae069a51
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-14 09:31:08 +0530

    commit

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -2,17 +2,59 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 
-export function AddMovie({ movies }) {
-  const [movie, setMovie] = useState([movies]);
-  console.log(movies);
+export function AddMovie({ List_of_Movies }) {
+  const [movies, setMovies] = useState(List_of_Movies);
+  
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("");
+  const [summary, setSummary] = useState("");
+  const [poster, setPoster] = useState("");
+  const addMovie = ()=> {
+  
+      const newMovie={
+        name: name,
+        rating: parseInt(rating),
+        summary: summary,
+        poster: poster,
+        
+      }
+  setMovies([ ...movies, newMovie])
+};
+  
   return (
     <div className='add-movie'>
-      <TextField id="standard-basic" label="Enter the movie name" variant="standard" />
-      <TextField id="standard-basic" label="Enter the movie rating" variant="standard" />
-      <TextField id="standard-basic" label="Enter the movie summary" variant="standard" />
-      <TextField id="standard-basic" label="Enter the movie poster" variant="standard" />
-      <Button onClick={() => setMovie([...movies, movie])} variant="outlined">AddMovie</Button>
+        <TextField           
+          value={name} 
+          onChange={(event) =>setName(event.target.value)} 
+          placeholder='Enter the name'
+          id="standard-basic"  
+          variant="standard" />
+     
+        <TextField           
+          value={rating} 
+          onChange={(event) =>setRating(event.target.value)} 
+          placeholder='Enter the rating'
+          id="standard-basic"  
+          variant="standard" />
+
+        <TextField            
+          value={summary} 
+          onChange={(event) =>setSummary(event.target.value)} 
+          placeholder='Enter the summary'
+          id="standard-basic"  
+          variant="standard" />
+
+        <TextField  
+          value={poster} 
+          onChange={(event) =>setPoster(event.target.value)} 
+          placeholder='Enter the poster'
+          id="standard-basic"  
+          variant="standard" />
+
+      <Button onClick={addMovie} variant="outlined">AddMovie</Button>
 
     </div>
   );
 }
+
+
